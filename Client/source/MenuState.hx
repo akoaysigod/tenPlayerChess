7d45944916071a38d4818f7bb06a6fb821ad6756
Tony <akoaysigod@gmail.com>
2014-08-10 17:36:48 -0400

    menu complete I guess

--- Client/source/MenuState.hx
+++ Client/source/MenuState.hx
@@ -7,20 +7,23 @@ import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
 import flixel.group.FlxGroup;
+import flixel.util.FlxPoint;
 
 class MenuState extends FlxState {
 	private var _title:FlxText;
 	private var _kings:FlxGroup;
+	private var _play:FlxSprite;
+	private var _highlight:Bool;
 
 	override public function create():Void {
-		FlxG.cameras.bgColor = 0xFFFFFFFF;
+		FlxG.cameras.bgColor = 0x34282C;
 
 		_title = new FlxText(0.0, 0.0, "Chess!", 100);
 		_title.y = _title.height - 75.0;
 		_title.x = (FlxG.width / 2.0) - (_title.fieldWidth / 2.0);
 		_title.alignment = "center";
-		_title.setBorderStyle(FlxText.BORDER_OUTLINE, 0xFFCC66, 1.0);
-		_title.color = 0x6698FF;
+		_title.setBorderStyle(FlxText.BORDER_OUTLINE, 0x2B65EC, 1.0);
+		_title.color = 0xFF8C00;
 		_title.antialiasing = true;
 		add(_title);
 
@@ -38,6 +41,14 @@ class MenuState extends FlxState {
 		}
 		add(_kings);
 
+		//FlxButton doesn't work on HTML target =(
+		//Neither does the MouseEventManager, yay =(
+		_play = new FlxSprite(0.0, FlxG.height / 2.0);
+		_play.loadGraphic("assets/images/joinGameButtonNormal.png");
+		_play.x = (FlxG.width / 2.0) - (_play.width / 2.0);
+		_highlight = false;
+		add(_play);
+
 		super.create();
 	}
 	
@@ -46,10 +57,46 @@ class MenuState extends FlxState {
 		_title = null;
 		_kings.destroy();
 		_kings = null;
+		_play = null;
 		super.destroy();
 	}
 
 	override public function update():Void {
+		var x = FlxG.mouse.x;
+		var y = FlxG.mouse.y;
+		var point = new FlxPoint(x, y);
+
+		if (_play.overlapsPoint(point)) {
+			if (!_highlight) {
+				_highlight = true;
+				_play.loadGraphic("assets/images/joinGameButtonHighlight.png");
+			}
+
+			if (FlxG.mouse.justPressed) {
+				_play.loadGraphic("assets/images/joinGameButtonPressed.png");
+			}
+			else if (FlxG.mouse.justReleased) {
+				joinGame();
+			}
+		}
+		else {
+			if (_highlight) {
+				_highlight = false;
+				_play.loadGraphic("assets/images/joinGameButtonNormal.png");
+			}
+		}
+
 		super.update();
 	}	
+
+	
+	private function joinGame():Void {
+		//transitions don't seem to do anything on HTML5
+		//FlxG.cameras.fade(0xFF00FF, 2.0, false, onFade, false);
+
+		var s:PlayState = new PlayState();
+		FlxG.switchState(s);
+	}
+
+	//private function onFade():Void {}
 }
